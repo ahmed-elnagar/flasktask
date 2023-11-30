8c996e83ad0481d1630a8a77e53f9e21f903ef76
ahmed-elnagar <55024226+ahmed-elnagar@users.noreply.github.com>
2023-11-30 18:54:15 +0200

    add 3 endpoints

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
@@ -7,7 +8,25 @@ def sum():
     reqJson = request.get_json()
     x = reqJson['x']
     y = reqJson['y']
-    sumVal = x+y
-    return {"message": f"sum of {x} and {y} is {sumVal}"}
+    result = x+y
+    return {"message": f"sum of {x} and {y} is {result}"}
 
+@app.route('/multiply' , methods=['POST'])
+def multiply():
+    reqJson = request.get_json()
+    x = reqJson['x']
+    y = reqJson['y']
+    result = x*y
+    return {"message": f"multiply of {x} and {y} is {result}"}
+
+
+@app.route('/divide' , methods=['POST'])
+def divide():
+    reqJson = request.get_json()
+    x = reqJson['x']
+    y = reqJson['y']
+    if y==0:
+        return {"error":"y can't be 0"} , 400
+    result = x/y
+    return {"message": f"division of {x} and {y} is {result}"}
 app.run(host='0.0.0.0', port=50100)
